c3e152d4cabf1f3a1a9404da40f64c727334320d
Jacobdeanr <Jacobdeanr@gmail.com>
2022-07-06 08:46:35 -0500

    Removed VMT modifications
    Disabled VMT modifications as this would delete the third line of every vmt file in the users materials/skybox/ folder.

--- Skybox_Builder.ps1
+++ Skybox_Builder.ps1
@@ -1,16 +1,18 @@
-﻿#--------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
 # Get the vtex and game directory... then cd there.
 #--------------------------------------------------------------------------------------------------------
 [string]$gameDir = Read-Host "enter path to gameinfo.txt directory. This should be similar to C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\hl2"
 cd $gameDir
 cd ..\bin
 $vtexDir = Get-Location
-$pfmFolder = Read-Host "Enter PFM Folder Directory. MUST BE placed in <anyfolder>\materialsrc\skybox sub folder."
-cd $pfmFolder
+$pfmFolder = Read-Host "Enter PFM Folder Directory. MUST BE placed in <anyfolder>\materialsrc\skybox sub folder"
+
 
 #--------------------------------------------------------------------------------------------------------
 # Make text files for all pfm files in folder directory.
 #--------------------------------------------------------------------------------------------------------
+cd $pfmFolder
+
 Get-ChildItem $pfmFolder -Filter "*.pfm" | Foreach-Object {
     $fileName = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
     New-Item -Force -Path . -Name "$fileName.txt" -value @"
@@ -25,42 +27,43 @@ Get-ChildItem $pfmFolder -Filter "*.pfm" | Foreach-Object {
 #--------------------------------------------------------------------------------------------------------
 # Make the vtf and vmt files
 # MUST USE $hdrcompressedtexture for the created skybox in the VMT.
-# TODO: Add a toggle for basetexturetransform
+# TODO: Add a toggle for basetexturetransform for half sized files.
 #--------------------------------------------------------------------------------------------------------
+cd $pfmFolder
 
 Get-ChildItem $pfmFolder -Filter "*.pfm" | Foreach-Object {
     $pfmfile = $_.Name
     $skyNameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
     $ldrskyname = $skyNameNoExt.replace('_hdr','')
-    $pfmfile
 
     if($ldrskyname.contains('dn') -or $ldrskyname.contains('up')){
-    & $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$baseTexture" "skybox/$ldrskyname" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile
+    & $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$baseTexture" "skybox/$ldrskyname" -vmtparam "`$nofog" "1" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile
     }
     else{
-    #& $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$baseTexture" "skybox/$ldrskyname" -vmtparam "`$basetexturetransform" "center 0 0 scale 1 2 rotate 0 translate 0 0" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile
-    & $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$baseTexture" "skybox/$ldrskyname" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile
+    # Switch these aroudn for 2:1 skyboxes. i.e. 1024x512
+    & $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$BaseTexture" "skybox/$ldrskyname" -vmtparam "`$nofog" "1" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile #Square Sides
+    # & $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$BaseTexture" "skybox/$ldrskyname" -vmtparam "`$basetexturetransform" "center 0 0 scale 1 2 rotate 0 translate 0 0" -vmtparam "`$nofog" "1" -vmtparam  "`$ignorez" "1" -vmtparam  "`$hdrcompressedtexture" "skybox/$skyNameNoExt" $pfmfile #2:1 Sides.
     }
 }
-
-cd $gamedir/materials/skybox/
-$sourceTextures = Get-Location
 
 #--------------------------------------------------------------------------------------------------------
 # Edit the vmt to remove duplicate basetexture in line 3.
 #--------------------------------------------------------------------------------------------------------
-Get-ChildItem $sourceTextures -Filter "*.vmt" | Foreach-Object {
-    $vmtfile = $_.Name
-    $skyNameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
-(Get-Content "$skyNameNoExt.vmt") | Where-Object ReadCount -ne 3 | Set-Content "$skyNameNoExt.vmt" -force
-}
+#cd $gamedir/materials/skybox/
+#$sourceTextures = Get-Location
+#
+#Get-ChildItem $sourceTextures -Filter "*.vmt" | Foreach-Object {
+#    $vmtfile = $_.Name
+#    $skyNameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+#    (Get-Content "$skyNameNoExt.vmt") | Where-Object ReadCount -ne 3 | Set-Content "$skyNameNoExt.vmt" -force
+#}
+#Disabled right now until I can think of a safer way to modify the VMT Files.
 
 
 #--------------------------------------------------------------------------------------------------------
 # Create LDR fallbacks.
 #--------------------------------------------------------------------------------------------------------
 cd $pfmFolder
-
 Get-ChildItem $pfmFolder -Filter "*.tga" | Foreach-Object {
     $tgafile = $_.Name
     $skyNameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
@@ -69,8 +72,9 @@ Get-ChildItem $pfmFolder -Filter "*.tga" | Foreach-Object {
     clamps 1
     clampt 1
 "@
-& $vtexDir/vtex.exe -game $gameDir -nopause $tgafile
+& $vtexDir/vtex.exe -game $gameDir -nopause -shader UnlitGeneric -vmtparam "`$ignorez" "1" -vmtparam "`$nofog" "1" $tgafile
 }
+
 
 #--------------------------------------------------------------------------------------------------------
 # Exit
@@ -78,7 +82,7 @@ Get-ChildItem $pfmFolder -Filter "*.tga" | Foreach-Object {
 Write-Host @"
 
 "@
-Write-Host "Closing in 10 seconds." -BackgroundColor Magenta
+Write-Host "Remember to modify your $BaseTexture parameter for HDR VMT Files!" -ForegroundColor Red
+Read-Host -Prompt "Completed. Press Enter to close"
 ii $gameDir/materials/skybox
-Start-Sleep -Seconds 10
 Exit
